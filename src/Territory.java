7b9eb7a1cf680a679d6b5b6bda1040f138f029e0
Vijay Ramalingom <vijayramalingom@cmail.carleton.ca>
2020-10-24 16:14:46 -0400

    Finished documenting territory

--- src/Territory.java
+++ src/Territory.java
@@ -11,51 +11,103 @@ public class Territory {
      * The player who owns the territory
      */
     private Player owner;
+
+    /**
+     * The number of armies that are on the territory
+     */
     private int numArmies;
+
+    /**
+     * The name of the territory
+     */
     private String name;
 
+    /**
+     * The constructor of territory sets neighbours to an empty array list, name to the value of the parameter and numArmies to 0
+     * @param name the name of the territory
+     */
     public Territory(String name){
         neighbours = new ArrayList<>();
         this.name = name;
         numArmies = 0;
     }
 
+    /**
+     * prints a string representation of the Territory
+     * @return the name of the territory with its owner and number of armies
+     */
     public String toString(){
         return name + "\nOwner: " + owner + "\nArmies: " + numArmies;
     }
 
+    /**
+     * Gets the neighbors of the territory
+     * @return the list of neighbors
+     */
     public List<Territory> getNeighbours() {
         return neighbours;
     }
 
+    /**
+     * adds a neighbor to the territory
+     * @param neighbour the neighbor which is added as a neighbor to the territory
+     */
     public void addNeighbour(Territory neighbour) {
         neighbours.add(neighbour);
     }
 
+    /**
+     * removes a neighbor from the territory
+     * @param neighbour the neighbor which is to be removed
+     */
     public void removeNeighbour(Territory neighbour) {
         neighbours.remove(neighbour);
     }
 
+    /**
+     * gets the owner of the territory
+     * @return the player who owns the territory
+     */
     public Player getOwner() {
         return owner;
     }
 
+    /**
+     * sets the owner of the territory
+     * @param owner the player who will own the territory
+     */
     public void setOwner(Player owner) {
         this.owner = owner;
     }
 
+    /**
+     * gets the number of armies that are on the territory
+     * @return the number of armies on the territory
+     */
     public int getNumArmies() {
         return numArmies;
     }
 
+    /**
+     * sets the number of armies on the territory
+     * @param numArmies the number of armies that will be on the territory
+     */
     public void setNumArmies(int numArmies) {
         this.numArmies = numArmies;
     }
 
+    /**
+     * adds armies to the territory
+     * @param numArmies the number of armies to be added to the territory
+     */
     public void addArmies(int numArmies) {
         this.numArmies += numArmies;
     }
 
+    /**
+     * gets the name of the territory
+     * @return the name of the territory
+     */
     public String getName(){
         return name;
     }
