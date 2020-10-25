801b2c22a60f0c20f57d4e1ede4d808b65473184
ljjb97 <ljjb97@gmail.com>
2020-10-24 22:40:22 -0400

    Added more comments to board

--- src/Board.java
+++ src/Board.java
@@ -1,55 +1,116 @@
 import java.util.*;
 
 public class Board {
+    /**
+     * A list of continents that belong to the board
+     */
     private List<Continent> continents;
+    /**
+     * A list of players that are playing on the board
+     */
     private List<Player> players;
+    /**
+     * The player who's turn it is currently
+     */
     private Player currentPlayer;
+    /**
+     * Number of armies the current player can place
+     */
     private int armiesToPlace;
+    /**
+     * The stage of the turn the current player is on
+     */
     private TurnStage turnStage;
+    /**
+     * The max number of dice rolls
+     */
     public static final int MAX_DICE_ROLL = 6;
 
+    /**
+     * Constructor for the board
+     */
     public Board(){
         continents = new ArrayList<>();
         players = new ArrayList<>();
     }
 
+    /**
+     * Get the current players turnstage
+     * @return current turnstage
+     */
     public TurnStage getTurnStage() {
         return turnStage;
     }
 
+    /**
+     * Goes to the next players turn and gives them armies
+     */
     public void goToNextTurn(){
         currentPlayer = players.get((players.indexOf(currentPlayer) + 1) % players.size());
         armiesToPlace = getArmyBonusForPlayer(currentPlayer);
     }
 
+    /**
+     * Sets turnStage to the next turn stage
+     */
     public void goToNextTurnStage(){
         turnStage = TurnStage.values()[(turnStage.ordinal() + 1) % TurnStage.values().length];
     }
 
+    /**
+     * Sets the objects turnStage to what is given
+     * @param turnStage the turnStage to go to
+     */
     public void setTurnStage(TurnStage turnStage) {
         this.turnStage = turnStage;
     }
 
+    /**
+     * Tells the user how many Armies they have yet to place
+     * @return the number of armies to place
+     */
     public int getArmiesToPlace() {
         return armiesToPlace;
     }
 
+    /**
+     * Increases the number of armies left to place by the input
+     * @param armiesToPlace number to add to the current number of armies to place
+     */
     public void addArmiesToPlace(int armiesToPlace) {
         this.armiesToPlace += armiesToPlace;
     }
 
+    /**
+     * Give the player currently stored in currentPlayer
+     * @return the currentPlayer
+     */
     public Player getCurrentPlayer() {
         return currentPlayer;
     }
 
+    /**
+     * Sets the current player to the one given
+     * @param currentPlayer player to set as currentPlayer
+     */
     public void setCurrentPlayer(Player currentPlayer) {
         this.currentPlayer = currentPlayer;
     }
 
+    /**
+     * Calculates how many armies to give to the specified player at the start of their turn
+     * @param player the player to calculate for
+     * @return the number of armies that player gets at the start of their turn
+     */
     public int getArmyBonusForPlayer(Player player){
         return Math.max((player.getNumTerritories() / 3) + getContinentBonusForPlayer(player), 3);
     }
 
+    /**
+     * Checks to see if the player controls any of the continents and returns the bonus' they get from that
+     * @param player the player to check
+     * @return the number of bonus armies the player gets
+     */
     private int getContinentBonusForPlayer(Player player){
         int sum = 0;
         for(Continent c: continents){
@@ -90,6 +151,11 @@ public class Board {
         return null;
     }
 
+    /**
+     * Gives the continent with the same name as the one specified
+     * @param name the name of the continent to search for
+     * @return the continent object that corresponds to the name given (null if name doesn't belong to a continent)
+     */
     public Continent findContinentByName(String name){
         for(Continent c: continents){
             if(c.getName().toLowerCase().equals(name.toLowerCase())) return c;
@@ -97,6 +163,11 @@ public class Board {
         return null;
     }
 
+    /**
+     * Give the player with the name equal to one given
+     * @param name the name to look for in the player list
+     * @return the player object with the same name (null if there is no player with that name)
+     */
     public Player findPlayerByName(String name){
         for(Player p: players){
             if(p.getName().toLowerCase().equals(name.toLowerCase())) return p;
@@ -117,16 +188,35 @@ public class Board {
         return list;
     }
 
+    /**
+     * Gives the list of players
+     * @return the list of players
+     */
     public List<Player> getPlayerList(){
         return players;
     }
 
+    /**
+     * Add a player to the player list
+     * @param player the player to add to the list
+     */
     public void addPlayer(Player player) {players.add(player);}
 
+    /**
+     * Add a continent to the continent list
+     * @param continent the continent to add to the list
+     */
     public void addContinent(Continent continent){
         continents.add(continent);
     }
 
+    /**
+     * Conducts a dice battle with the specified number of dice. Positive return values indicate that the attacker
+     * won the battle. Negative values indicate the defender has won.
+     * @param attackerDiceNum The number of dice the attacker will use
+     * @param defenderDiceNum The number of dice the defender will use
+     * @return the result of the dice battle (positive indicates attacker won, negative indicates defender won)
+     */
     public static int attackResult(int attackerDiceNum, int defenderDiceNum){
 
         int result = 0;
@@ -144,6 +234,7 @@ public class Board {
             int topDefendDie = Collections.max(defendDice);
             attackDice.remove((Integer) topAttackDie);
             defendDice.remove((Integer) topDefendDie);
+            //if the attack die is larger than the defend die then add 1 else subtract 1 from result
             result += topAttackDie > topDefendDie? 1: -1;
         }
 
