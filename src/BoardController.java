33bd390b1db8dcf255b356800334c0d9d7bf7f24
benbaggs <benbaggs@cmail.carleton.ca>
2020-12-04 22:40:04 -0500

    - territories from previous maps will be erased on screen when the user starts playing on a different map

--- src/BoardController.java
+++ src/BoardController.java
@@ -59,6 +59,9 @@ public class BoardController {
         } while(!board.isValid());
 
         board.addRiskView(boardView);
+        //
+        boardView.repaint();
+        //
         board.nextTurnStage();
     }
 
@@ -70,6 +73,9 @@ public class BoardController {
         } while(!board.isValid());
 
         board.addRiskView(boardView);
+        //
+        boardView.repaint();
+        //
         board.nextTurnStage();
     }
 
